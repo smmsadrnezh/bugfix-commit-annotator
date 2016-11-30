80a68de2ddb6d512a7e56928caae3dcb3938c636
Masoud Sadrnezhaad <masoud@sadrnezhaad.ir>
2016-11-30 21:38:37 +0330

    finding diffEntries in each commit

--- src/CommitFinder.java
+++ src/CommitFinder.java
@@ -2,11 +2,16 @@ import org.eclipse.jgit.api.BlameCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.blame.BlameResult;
-import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.diff.DiffFormatter;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.treewalk.CanonicalTreeParser;
+import org.eclipse.jgit.util.io.DisabledOutputStream;
 
 import java.io.File;
 import java.io.IOException;
@@ -27,6 +32,7 @@ public class CommitFinder {
     private BlameCommand blamer = new BlameCommand(repository);
     private ArrayList<RevCommit> bugfixCommits = new ArrayList<>();
     private HashMap<RevCommit, ArrayList<RevCommit>> result = new HashMap<>();
+    private List<DiffEntry> diffEntries;
 
     CommitFinder(String path) {
         this.path = path;
@@ -72,21 +78,39 @@ public class CommitFinder {
         }
     }
 
-    void annotateRevisions() throws GitAPIException {
+    void annotateRevisions() throws GitAPIException, IOException {
 
-        for (RevCommit commit : bugfixCommits) {
+        for (RevCommit bugfixCommit : bugfixCommits) {
 
-            String changedFilePath = "README.md";
-            int lineNumber = 0;
+            ObjectReader reader = repository.newObjectReader();
 
-            blamer.setFilePath(changedFilePath);
-            blamer.setStartCommit(commit.getId());
-            BlameResult blame = blamer.call();
-            RevCommit annotationCommit = blame.getSourceCommit(lineNumber);
-            ArrayList<RevCommit> annotateBugyCommits = new ArrayList<>();
-            annotateBugyCommits.add(annotationCommit);
+            CanonicalTreeParser oldTreeIter = new CanonicalTreeParser();
+            ObjectId oldTree = bugfixCommit.getTree();
+            oldTreeIter.reset(reader, oldTree);
 
-            result.put(commit, annotateBugyCommits);
+            CanonicalTreeParser newTreeIter = new CanonicalTreeParser();
+            ObjectId newTree = bugfixCommit.getParent(0).getTree();
+            newTreeIter.reset(reader, newTree);
+
+            DiffFormatter diffFormatter = new DiffFormatter(DisabledOutputStream.INSTANCE);
+            diffFormatter.setRepository(repository);
+            diffEntries = diffFormatter.scan(oldTreeIter, newTreeIter);
+
+            for (DiffEntry diffEntry : diffEntries) {
+                if (diffEntry.getChangeType().toString() == "MODIFY") {
+                    String changedFilePath = diffEntry.getPath(DiffEntry.Side.NEW);
+                    int lineNumber = 0;
+
+                    blamer.setFilePath(changedFilePath);
+                    blamer.setStartCommit(bugfixCommit.getId());
+                    BlameResult blame = blamer.call();
+                    RevCommit annotationCommit = blame.getSourceCommit(lineNumber);
+                    ArrayList<RevCommit> annotateCommits = new ArrayList<>();
+                    annotateCommits.add(annotationCommit);
+
+                    result.put(bugfixCommit, annotateCommits);
+                }
+            }
 
         }
     }
